5ad62760f7ae1fd959645091e3c8a13316682279
OAT <64010860@kmitl.ac.th>
2021-10-17 20:31:11 +0700

    Add project files.

---
+++ ProblemOfTheWeek/57method1.cpp
@@ -0,0 +1,16 @@
+﻿// 57. จงเขียนโปรแกรมรับ string มา 1 ชุด แล้วหาว่าตัวอักษรใดที่เป็นตัวอักษรหลังสุด (Level 3)
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string.h>
+
+int main()
+{
+	char sen[1000];
+	scanf("%s", sen);
+	int length = strlen(sen);
+	printf("%c", sen[length-1]);
+
+
+	return 0;
+}
---
+++ ProblemOfTheWeek/57method2.cpp
@@ -0,0 +1,19 @@
+﻿// 57. จงเขียนโปรแกรมรับ string มา 1 ชุด แล้วหาว่าตัวอักษรใดที่เป็นตัวอักษรหลังสุด (Level 3)
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+
+
+int main()
+{
+	char sen[1000];
+	scanf("%s", sen);
+	int i = 0;
+	while (sen[i + 1] != '\0')
+	{
+		i++;
+	}
+	printf("%c", sen[i]);
+
+	return 0;
+}
